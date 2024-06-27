b8b4c220f3a93c5c8b77a68428ff82a1ebb1179e
TanmayVidhate <tanmayvidhate7@gmail.com>
2024-06-27 10:27:11 +0530

    stored value  in other variables

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const Text1 = document.getElementById("input-field1")
 const Text2 = document.getElementById("input-field2")
 const result = document.getElementById("Result")
+
+const txt1 = Text1
+const txt2 = Text2
+const res = result
